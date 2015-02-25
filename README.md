1b718c31bc41b53d53f9ea06a65d0d7d634ccd40
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2015-02-25 15:06:15 -0700

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # bootstrap-sandbox
+
+This project is used to assist in the troubleshooting of Bootstrap problems.
---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<?php require_once("lib/head-utils.html"); ?>
+		<title>Bootstrap Sandbox</title>
+	</head>
+	<body>
+
+		<!--header include-->
+		<?php require_once("lib/header.php"); ?>
+
+		<main>
+
+
+
+		</main>
+
+		<!--footer include-->
+		<?php require_once("lib/footer.php"); ?>
+
+	</body>
+</html>
