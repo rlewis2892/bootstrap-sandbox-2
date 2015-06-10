9a610e274f00d86b7d17bd870d044a1889a3bd0b
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2015-06-10 10:07:05 -0600

    added codewizard feature branch

--- index.php
+++ index.php
@@ -1,22 +1,97 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<?php require_once("lib/head-utils.html"); ?>
-		<title>Bootstrap Sandbox</title>
+		<meta charset="utf-8">
+		<!-- Bootstrap Latest compiled and minified CSS -->
+		<link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet"/>
+
+		<!-- Bootstrap theme -->
+		<link type="text/css" href="//maxcdn.bootstrapcdn.com/bootswatch/3.3.4/superhero/bootstrap.min.css" rel="stylesheet"/>
+
+		<!--	my own stylings-->
+		<link type="text/css" href="css/codewizard.css" rel="stylesheet"/>
+
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+		<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
+		<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.13.1/jquery.validate.min.js"></script>
+		<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.13.1/additional-methods.min.js"></script>
+		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.3/jquery-ui.min.js"></script>
+
+		<!-- Latest compiled and minified Bootstrap JavaScript, all compiled plugins included -->
+		<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+
+		<!--			Our own javascript/ajax calls-->
+		<script type="text/javascript" src="js/js.js"></script>
+		<script type="text/javascript" src="js/gallery.js"></script>
+
+		<title>wizards winsome weblies</title>
 	</head>
 	<body>
+		<header>
+			<div class="row text-center col-xs-5 col-xs-offset-3 col-md-4 col-md-offset-4"><h1>thecodewizard</h1></div>
+		</header>
 
-		<!--header include-->
-		<?php require_once("lib/header.html"); ?>
-
-		<main>
-			<div class="container">
-				Main Content Here
+		<main class="container-fluid">
+			<div class="row magicmargin">
+				<img src="img/transparentwizard.png" alt="wizardholder" id="middlewizard" class="img-responsive img-circle img-center" />
 			</div>
 		</main>
 
-		<!--footer include-->
-		<?php require_once("lib/footer.html"); ?>
+		<section id="photography">
+			<div class="container section-breaker col-md-offset-1 col-md-8">
+				<a name="photography"></a><h2 class="text-left">Photography</h2>
+				<p class="text-left">I spent around four years in the Seattle, WA area. Here are some of my favorite photos I took during the time. Sony mirrorless cameras! Woo!</p>
+				<div class="row">
+					<div class="col-lg-4 col-sm-12 col-xs-12"><a title="Image 3" href="https://goo.gl/photos/My3U1WJ9WM5aUtvo9"><img class="thumbnail img-responsive" src="img/photography/photo%20-%203.jpg"></a></div>
+					<div class="col-lg-4 col-sm-12 col-xs-12"><a title="Image 9" href="https://goo.gl/photos/My3U1WJ9WM5aUtvo9"><img class="thumbnail img-responsive" src="img/photography/photo%20-%209.jpg"></a></div>
+					<div class="col-lg-4 col-sm-12 col-xs-12"><a title="Image 22" href="https://goo.gl/photos/My3U1WJ9WM5aUtvo9"><img class="thumbnail img-responsive" src="img/photography/photo%20-%2022.jpg"></a></div>
+				</div>
+			</div>
+		</section>
+
+		<section id="gaming">
+			<div class="container section-breaker col-md-offset-6 col-md-5">
+				<a name="gaming"></a><h2 class="text-right">Gaming</h2>
+				<p class="text-right">This is Heroes of the Storm, the Blizzard MOBA. My first pentakill as Gazlowe!</p>
+				<iframe class="pull-right" width="560" height="315" src="https://www.youtube.com/embed/PoFQFHzPREQ" frameborder="0" allowfullscreen></iframe>
+			</div>
+		</section>
+
+		<section id="videography">
+			<div class="container section-breaker col-md-offset-1 col-md-8">
+				<h2 class="text-left">Videography</h2>
+				<p class="text-left">This is a video I created from a photo shoot with Sony Vegas.</p>
+				<div class="pull-left">
+					<iframe width="560" height="315" src="https://player.vimeo.com/video/103035213?portrait=0" frameborder="0" allowfullscreen></iframe>
+				</div>
+			</div>
+		</section>
+
+		<section id="music">
+			<div class="container section-breaker col-md-8 col-md-offset-3">
+				<a name="music"></a><h2 class="text-right">Music</h2>
+				<p class="text-right">I've been in a few bands over the years. It's been great!</p>
+				<div class="row">
+					<div class="col-lg-4 col-sm-12 col-xs-12"><a title="Country Band Pic 1" href="#music"><img class="thumbnail img-responsive" src="img/band/country1.jpg"></a></div>
+					<div class="col-lg-4 col-sm-12 col-xs-12"><a title="Country Band Pic 2" href="#music"><img class="thumbnail img-responsive" src="img/band/country2.jpg"></a></div>
+					<div class="col-lg-4 col-sm-12 col-xs-12"><a title="Country Band Pic 3" href="#music"><img class="thumbnail img-responsive" src="img/band/country3.jpg"></a></div>
+				</div>
+				<div class="row">
+					<div class="col-lg-4 col-sm-12 col-xs-12"><a title="Teenage Rock Band 1" href="#music"><img class="img-rounded img-responsive" src="img/band/dr1.jpg"></a></div>
+					<div class="col-lg-4 col-sm-12 col-xs-12"><a title="Teenage Rock Band 2" href="#music"><img class="img-rounded img-responsive" src="img/band/dr2.jpg"></a></div>
+					<div class="col-lg-4 col-sm-12 col-xs-12"><a title="Teenage Rock Band 3" href="#music"><img class="img-rounded img-responsive" src="img/band/dr3.jpg"></a></div>
+				</div>
+				<div class="row">
+					<div class="col-lg-4 col-sm-12 col-xs-12"><a title="Oldies Band 1" href="#music"><img class="thumbnail img-responsive" src="img/band/rrb1.jpg"></a></div>
+					<div class="col-lg-4 col-sm-12 col-xs-12"><a title="Oldies Band 2" href="#music"><img class="thumbnail img-responsive" src="img/band/rrb2.jpg"></a></div>
+				</div>
+			</div>
+		</section>
+
+		<footer>
+
+		</footer>
 
 	</body>
 </html>
