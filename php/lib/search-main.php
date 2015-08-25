36ea4983084a2515e4104d670470b309a768dee7
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2015-08-25 09:39:11 -0600

    add existing code from github

--- php/lib/search-main.php
+++ php/lib/search-main.php
@@ -1,7 +1,26 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: ro
- * Date: 8/25/15
- * Time: 9:28 AM
- */
+<div class="main-text">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h1 class="search-main-h1">Find a delicious, clean place to eat.</h1>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<form id="main-search" method="get" action="">
+					<div class="main-search-align">
+						<div class="input-group">
+							<input id="mainSearch" name="mainSearch" type="text" class="form-control main-search"
+									 placeholder="Search in Albuquerque">
+							<span class="input-group-btn">
+								<button id="restaurant-search-submit" type="submit" class="btn main-search-button">
+									<i class="fa fa-search"></i>
+								</button>
+							</span>
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+</div>
