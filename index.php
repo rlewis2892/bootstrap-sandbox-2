8a80da86cf15c7cc1b996da08c794f69efa8352e
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2017-03-06 13:32:59 -0700

    recreate abquery splash page with simplified implementation

--- index.php
+++ index.php
@@ -6,8 +6,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 
 		<!-- Latest compiled and minified CSS -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
-				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
 		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
 		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -17,8 +16,7 @@
 		<![endif]-->
 
 		<!-- Font Awesome -->
-		<link type="text/css" href="//maxcdn.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css"
-				rel="stylesheet"/>
+		<link type="text/css" href="//maxcdn.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css" rel="stylesheet"/>
 
 		<!-- Custom CSS -->
 		<link rel="stylesheet" href="css/style.css" type="text/css">
@@ -35,40 +33,50 @@
 	</head>
 	<body class="sfooter">
 		<main class="sfooter-content">
+
 			<div class="logo-search">
-				<div class="logo text-center">
-					<img class="logo-resize" src="images/abqueryname.png" alt="abquery logo">
-				</div>
-				<div class="container text-center">
+				<div class="container">
 					<div class="row">
-						<div class="col-sm-6 col-sm-offset-3">
-							<!-- <h2 class="search-text">Please Input an ABQ Address</h2> -->
-							<div id="custom-search-input">
-								<div class="input-group col-md-12">
-									<input type="text" class="form-control input-lg" placeholder="ABQ Address"/>
-									<span class="input-group-btn">
-                        <button class="btn btn-info btn-lg" type="button">
-                            <i class="glyphicon glyphicon-search"></i>
-                        </button>
-                    </span>
+						<div class="col-sm-6 col-sm-offset-6 col-md-4 col-md-offset-4 text-center">
+							<img class="img-responsive" src="images/abqueryname.png" alt="abquery logo">
+
+							<!-- search field -->
+							<form>
+								<div class="form-group">
+									<label for="searchAddress" class="sr-only">Search an Albuqueruqe Address</label>
+									<div class="input-group">
+										<input id="searchAddress" name="searchAddress" type="text" class="form-control input-lg" placeholder="ABQ Address"/>
+										<div class="input-group-btn">
+											<button class="btn btn-info btn-lg" type="button">
+												<i class="glyphicon glyphicon-search"></i>
+											</button>
+										</div>
+									</div>
 								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
+							</form>
+
+						</div><!--/.col-*-->
+					</div><!--/.row-->
+				</div><!--/.container-->
+			</div><!--/.logo-search-->
+
+			<!-- background video -->
 			<video autoplay loop muted poster="images/screenshot.jpg" id="background">
 				<source src="images/backgroundmovie.mp4" type="video/mp4">
 			</video>
+
 		</main>
-		<footer class="text-center">
-			<div class="container foot-content">
+
+		<footer>
+			<div class="container text-center">
 				<div class="row">
 					<div class="col-md-4">
 						<p class="text-muted copy">Copyright &copy; ABQuery 2017</p>
 					</div>
 					<div class="col-md-4 col-md-offset-4">
-						<a href="about-view.php"><p class="text-muted copy">About Us</p></a>
+						<p class="text-muted copy">
+							<a href="about-view.php">About Us</a>
+						</p>
 					</div>
 				</div>
 			</div>
